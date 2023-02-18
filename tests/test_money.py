21d15c59224af43125e5039c52b5bc12b0a1963a
Connor <conkeating32@gmail.com>
2023-02-18 17:03:44 -0500

    Renaming variables to make it clear why this behavior is bad

--- tests/test_money.py
+++ tests/test_money.py
@@ -6,8 +6,8 @@ from tdd.money import Dollar
 
 def test_multiplication():
     """Testing Dollar multiplication function."""
-    dollar = Dollar(amount=5)
-    dollar.times(factor=2)
-    assert dollar.amount == 10
-    dollar.times(3)
-    assert dollar.amount == 15
+    five: Dollar = Dollar(amount=5)
+    five.times(factor=2)
+    assert five.amount == 10
+    five.times(3)
+    assert five.amount == 15
