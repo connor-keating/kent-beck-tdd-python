f88a49055a72ed826c11f8664b5bb594f107f155
Connor <conkeating32@gmail.com>
2023-02-18 15:39:08 -0500

    Adding test docstring

--- tests/test_money.py
+++ tests/test_money.py
@@ -5,6 +5,7 @@ from tdd import money
 
 
 def test_multiplication():
+    """Testing Dollar multiplication function."""
     dollar = money.Dollar(amount=5)
     dollar.times(factor=2)
     assert dollar.amount == 10
